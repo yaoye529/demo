99f983ed929e09cb2ad7734f98eb5666edf832c5
zhuqiqiang <1042800345@qq.com>
2018-11-27 22:34:07 +0800

    修改配置文件，添加redis相关配置

--- src/test/java/com/qq/zhu/demo/ApplicationTests.java
+++ src/test/java/com/qq/zhu/demo/ApplicationTests.java
@@ -1,16 +1,28 @@
 package com.qq.zhu.demo;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringRunner.class)
+@ActiveProfiles("junit")
+@RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 public class ApplicationTests {
 
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
     @Test
-    public void contextLoads() {
+    public void test() throws Exception {
+        stringRedisTemplate.opsForValue().set("aaa", "111");
+        Assert.assertEquals("111", stringRedisTemplate.opsForValue().get("aaa"));
+
     }
 
 }
